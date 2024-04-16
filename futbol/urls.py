5fbd5891eb5c79b848a28af8fb2a59989022ee06
mauricioatm <mauriciochasi@gmail.com>
2024-04-16 13:38:20 +0200

    desarrolando vistas mas avanzadas

--- futbol/urls.py
+++ futbol/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import IndexView
 
 
-
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('equipos/<int:equipo_id>/', IndexView.equipo_detalle, name='equipo_detalle'),
+    path('', IndexView.acceso_home, name='index'),
+    path('equipos/<str:nombre_equipo>/',IndexView.equipo_detalle, name='equipo_detalle'),
 ]
